2e3301b57e4c5628a6e764b8500340dc79fe11cb
russellknox84 <russell.knox@digital.homeoffice.gov.uk>
2017-09-06 20:25:35 +0100

    Add new file so github will change cached file

---
+++ apps/ukvi_service_options/fields/what-do-you-want-to-do-in-the-uk.1.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+    'what-do-you-want-to-do-in-the-UK': {
+        validate: 'required',
+        mixin: 'checkbox-group',
+        legend: {
+            className: 'legend-padding'
+        },
+        'options': {
+            'family': {
+                'label': 'Live with my family',
+                'value': 'family'
+            },
+            'retire': {
+                'label': 'Retire',
+                'value': 'retire'
+            },
+            'business': {
+                'label': 'Start a business',
+                'value': 'business'
+            },
+            'study': {
+                'label': 'Study',
+                'value': 'study'
+            },
+            'volunteer for a charity': {
+                'label': 'Volunteer for a charity',
+                'value': 'charity'
+            },
+            'work': {
+                'label': 'Work',
+                'value': 'working'
+            }
+        }
+    }
+};
